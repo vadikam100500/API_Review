d7e12dd64d0a7f9da68502ac5f106bde392736b6
Robert-byte-ai <saakyan-robert@rambler.ru>
2021-08-09 23:02:56 +0300

    Add models Category,Genre,Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200)
+
+    slug = models.SlugField(
+        unique=True,
+        max_length=50)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200)
+
+    slug = models.SlugField(
+        unique=True,
+        max_length=50)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=200)
+
+    year = models.PositiveIntegerField(
+        'Год')
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles',
+        verbose_name='Категория')
+
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='titles',
+        blank=True,
+        verbose_name='Жанр')
+
+    description = models.TextField(
+        verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+        ordering = ('-year',)
+
+    def __str__(self):
+        return self.name[:50]
