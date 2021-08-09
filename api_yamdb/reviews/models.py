586624b4e07e986d7d9f956117afe4946a3651d9
Виталий <72@t5.ru>
2021-08-10 00:13:12 +0500

    Add models Review,Comments

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+User = get_user_model()
+
+class Review(models.Model):
+    title_id = models.ForeignKey(
+        "Title", on_delete=models.CASCADE,
+        related_name="reviews", verbose_name='Произведениe'
+    )
+    text = models.TextField(verbose_name='Отзыв')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    score = models.IntegerField(
+        verbose_name='Оценка',
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True,
+                                     blank=True)
+
+    class Meta:
+        ordering = ('-pub_date',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title_id', 'author'],
+                name='unique_name_reviews'
+            )
+        ]    
+    
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    review_id = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField(verbose_name='Коментарий')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True,  blank=True)
+
+    class Meta:
+        ordering = ('-pub_date',)    
+
+    def __str__(self):
+        return self.text            
